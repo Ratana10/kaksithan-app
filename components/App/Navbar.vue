b6433313cb9ba84bf14bb56217a3ddb86f5a9929
Ratana10 <sanratana18@gmail.com>
2024-03-26 19:29:06 +0700

    update navbar

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -1,11 +1,15 @@
 <template>
    <header class="p-4">
       <nav class="container-fluid flex justify-between items-center">
-         <a class="uppercase text-primary" href="">Kaksithan</a>
+         <NuxtLink class="uppercase text-primary text-xl" to="/">
+            <p>Kaksithan</p>
+         </NuxtLink>
          <div class="flex space-x-4">
             <ul class="flex space-x-4 items-center justify-center">
                <li v-for="(item, index) in navbar" :key="index">
-                  <NuxtLink class="uppercase" to="">{{ item.name }}</NuxtLink>
+                  <NuxtLink class="uppercase" :to="item.href">{{
+                     item.name
+                  }}</NuxtLink>
                </li>
             </ul>
             <div class="flex">
@@ -23,6 +27,7 @@
 const navbar = [
    {
       name: "Login",
+      href: "/contact",
    },
    {
       name: "Register",
