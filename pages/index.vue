6d9d7f913fa95327517f8628452f1048a5c1096f
Ratana10 <sanratana18@gmail.com>
2024-03-29 11:17:56 +0700

    update link

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="mt-10 flex items-center justify-center">
-      <NuxtLink to="/contact" class="py-4 px-8 bg-primary"
-         >Go to Contact Page</NuxtLink
+      <NuxtLink to="/contactus" class="py-4 px-8 bg-primary"
+         >Go to ContactUs Page</NuxtLink
       >
    </div>
 </template>
